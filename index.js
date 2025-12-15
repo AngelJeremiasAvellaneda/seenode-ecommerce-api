212bca08cc54d3db5ff67353b5cf5de4f0ae9737
AngelJeremiasAvellaneda <75193774@continental.edu.pe>
2025-12-15 14:50:29 -0500

    Arreglando Array

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.get('/products', (req, res) => {
-    res.json({
-        id: 1,
-        name: 'Laptop gaming',
-        price: 1000,
-        description: 'Laptop gaming de ultima generacion',
-        category: 'Laptop',
-        stock: 10,
-        imageURL: 'https://i.rtings.com/assets/pages/6dRuEBex/best-gaming-laptops-20242028-medium.jpg?format=auto'
-    })
+    res.json([
+        {
+            id: '1',
+            name: 'Laptop gaming',
+            price: 1000,
+            description: 'Laptop gaming de ultima generacion',
+            category: 'Laptop',
+            stock: 10,
+            imageURL: 'https://i.rtings.com/assets/pages/6dRuEBex/best-gaming-laptops-20242028-medium.jpg?format=auto'
+        }
+    ])
 })
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
